449cb118b8247b496809c4e51d28e6644174780a
jmajpopinf <jmajpopinf@gmail.com>
2024-05-17 01:50:32 +0000

    menu y foto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, CssBaseline, ThemeProvider, ToggleButton, ToggleButtonGroup, createTheme } from '@mui/material';
+import React from 'react';
+import getLPTheme from './getLPTheme';
+import PropTypes from 'prop-types';
+import AppAppBar from './components/AppAppBar';
+import Hero from './components/Hero';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme}){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100vw',
+        position: 'fixed',
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color='primary'
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label='Platform'
+        sx={{
+          backgroundColor: 'background.default',
+          '& .Mui-selected': {
+           PointerEvent: 'none',
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1}} />
+          Custom theme
+        </ToggleButton>
+        <ToggleButton value={false}> Material Design 2 </ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
   )
 }
 
-export default App
+ToggleCustomTheme.propTypes = {
+  showCustomTheme: PropTypes.shape({
+    valueOf: PropTypes.func.isRequired,
+  }).isRequired,
+  toggleCustomTheme: PropTypes.func.isRequired,
+};
+
+function App() {
+  const [mode, setMode] = React.useState('light');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(false);
+  const LPtheme = createTheme(getLPTheme(mode));
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const toggleColorMode  = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
+  //https://github.com/mui/material-ui/blob/v5.15.18/docs/data/material/getting-started/templates/landing-page/LandingPage.js
+  return (
+    <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+      <CssBaseline />
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode}/>
+      <Hero />
+    </ThemeProvider>
+  );
+}
+
+export default App;
